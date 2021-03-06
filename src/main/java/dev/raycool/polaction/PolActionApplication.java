3a49d802e10d5f39099dc9ef1751c8309be06ed4
stainlessray <raymondcoolmail@gmail.com>
2021-03-06 11:21:19 -0500

    completed serialization of new models

--- src/main/java/dev/raycool/polaction/PolActionApplication.java
+++ src/main/java/dev/raycool/polaction/PolActionApplication.java
@@ -14,6 +14,12 @@ import org.springframework.context.annotation.Bean;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.client.RestTemplate;
 
+//todo
+// Implement error handling for template population actions.
+// directly handle with try, catch, finally.
+// user flow may need to offer less refined results to users who experience problems.
+//
+
 @SpringBootApplication
 public class PolActionApplication implements CommandLineRunner {
 
@@ -40,8 +46,10 @@ public class PolActionApplication implements CommandLineRunner {
 
 	@Override
 	public void run(String... args) throws Exception {
-		String googleApiKey = "AIzaSyB-tUdCgR_eX1pDG2E2xIVDTqPZzto90ac";
-		String address = "39.674768, -75.6592079";
+		String googleApiKey = "";
+		//String address = "39.278171, -75.602260"; //39.278171, 75.602260
+		String address = "538 Barley Ct, Smyrna, Delaware, 19977";
+
 		String formattedGoogleApiUrl = String.format("https://www.googleapis.com/civicinfo/v2/representatives/?&address=%s&includeOffices=true&key=%s", address, googleApiKey);
 
 		ResponseEntity<PoliticalOfficialsResponse> response = restTemplate.getForEntity(formattedGoogleApiUrl, PoliticalOfficialsResponse.class);
@@ -51,6 +59,7 @@ public class PolActionApplication implements CommandLineRunner {
 		for(PoliticalOfficial politicalOfficial : allOfficials) {
 			logger.info(politicalOfficial.toString());
 			logger.info(politicalOfficial.getName());
+			logger.info(politicalOfficial.getAddress()[0].toString());
 			logger.info(politicalOfficial.getAddress()[0].getLine1());
 			logger.info(politicalOfficial.getAddress()[0].getCity());
 			logger.info(politicalOfficial.getAddress()[0].getState());
@@ -62,12 +71,22 @@ public class PolActionApplication implements CommandLineRunner {
 			}
 			logger.info("\n");
 
-			ResponseEntity<PoliticalOfficesResponse> response2 = restTemplate2.getForEntity(formattedGoogleApiUrl, PoliticalOfficesResponse.class);
-			PoliticalOffice[] allOffices = response2.getBody().getOffices();
-			logger.info("\n" + "++++++++++++++++++++++++++++++++" + "\n");
-			System.out.println(allOffices[0].toString());
 
+		}
+		ResponseEntity<PoliticalOfficesResponse> response2 = restTemplate2.getForEntity(formattedGoogleApiUrl, PoliticalOfficesResponse.class);
+		PoliticalOffice[] allOffices = response2.getBody().getOffices();
+		logger.info("\n" + "++++++++++++++++++++++++++++++++" + "\n");
+		logger.info(allOffices[0].getLevels()[0].getLevel());
+		for(PoliticalOffice politicalOffice : allOffices) {
+			logger.info(politicalOffice.getName());
+			logger.info(politicalOffice.getDivisionId());
 
+			logger.info("length of indices array = " + politicalOffice.getOfficialIndices().length);
+			logger.info(String.valueOf(politicalOffice.getOfficialIndices()[0].getOfficialIndices()));
+			if (politicalOffice.getOfficialIndices().length == 2) {
+				logger.info(String.valueOf(politicalOffice.getOfficialIndices()[1].getOfficialIndices()));
+			}
+			logger.info("\n");
 
 		}
 	}
--- src/main/java/dev/raycool/polaction/officesmodels/PoliticalOffice.java
+++ src/main/java/dev/raycool/polaction/officesmodels/PoliticalOffice.java
@@ -9,7 +9,7 @@ public class PoliticalOffice {
 
     private String name;
     private String divisionId;
-    //private Level[] levels;
+    private Level[] levels;
     private Role[] roles;
     private OfficialIndex[] officialIndices;
 
@@ -29,13 +29,13 @@ public class PoliticalOffice {
         this.divisionId = divisionId;
     }
 
-/*    public Level[] getLevels() {
+    public Level[] getLevels() {
         return levels;
     }
 
     public void setLevels(Level[] levels) {
         this.levels = levels;
-    }*/
+    }
 
     public Role[] getRoles() {
         return roles;
