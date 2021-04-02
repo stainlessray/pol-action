24c41a87d7abdb4cf1785e1d263513f93286373e
stainlessray <raymondcoolmail@gmail.com>
2021-04-02 09:51:32 -0400

    added model class with equals and hash

--- src/main/java/dev/raycool/polaction/PolActionApplication.java
+++ src/main/java/dev/raycool/polaction/PolActionApplication.java
@@ -3,6 +3,11 @@ package dev.raycool.polaction;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
+@SpringBootApplication
+public class PolActionApplication  {
 
+    public static void main(String[] args) {
+        SpringApplication.run(PolActionApplication.class, args);
+    }
 
-
+}
