7607affb882dea920a586b7567c5b1db21858955
Raymond <raymondcoolmail@gmail.com>
2021-10-04 20:15:31 -0400

    ...

--- src/main/java/dev/raycool/polaction/PolActionApplication.java
+++ src/main/java/dev/raycool/polaction/PolActionApplication.java
@@ -6,6 +6,10 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 @SpringBootApplication
 public class PolActionApplication  {
 
+
+    /*
+    *  Need the properties file from the Mac to run 6-24-01 11:22pm
+    * */
     public static void main(String[] args) {
         SpringApplication.run(PolActionApplication.class, args);
     }
